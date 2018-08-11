e199995b640d87a0e64492a4828a35b79e10a38f
zmichaels <zmichaels11@gmail.com>
2018-08-10 17:01:40 -0700

    Added code for inspection of Modules

--- petshop/src/main/java/petshop/DynamicAnimalTest.java
+++ petshop/src/main/java/petshop/DynamicAnimalTest.java
@@ -2,8 +2,12 @@ package petshop;
 
 import animals.Animal;
 
+import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.Set;
 
@@ -17,8 +21,41 @@ public class DynamicAnimalTest {
         var cfg = parent.configuration().resolve(finder, ModuleFinder.of(), Set.of("animals.barn", "animals.house"));
         var pluginLayer = parent.defineModulesWithOneLoader(cfg, ClassLoader.getSystemClassLoader());
 
+        System.out.println("Name of all plugins defined:");
+        pluginLayer.modules().stream()
+                .map(Module::getDescriptor)
+                .map(ModuleDescriptor::provides)
+                .flatMap(Set::stream)
+                .map(ModuleDescriptor.Provides::providers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Initialize Plugin from Module");
+        pluginLayer.findModule("animals.barn")
+                .map(module -> Class.forName(module, "animals.barn.Sheep"))
+                .flatMap(clazz -> {
+                    try {
+                        return Optional.of(clazz.getConstructor().newInstance());
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        return Optional.empty();
+                    }
+                })
+                .flatMap(plugin -> {
+                    if (plugin instanceof Animal) {
+                        return Optional.of((Animal) plugin);
+                    } else {
+                        return Optional.empty();
+                    }
+                })
+                .map(Animal::say)
+                .ifPresent(theSheep -> System.out.printf("The sheep say %s%n", theSheep));
+
+
         var petLoader = ServiceLoader.load(pluginLayer, Animal.class);
 
+        System.out.println();
+        System.out.println("SPI loaded Plugins:");
         for (var animal : petLoader) {
             System.out.printf("The %s says %s%n", animal.getClass().getSimpleName(), animal.say());
         }
